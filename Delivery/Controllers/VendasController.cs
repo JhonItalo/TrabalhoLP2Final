65b844683ff22723f7b5dcf4e9e2ea3a7221662d
Jhon Italo <Jhon2italo@gmail.com>
2019-04-19 23:30:12 -0300

    Resetando alterações in the table

---
+++ Delivery/Controllers/VendasController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delivery.Controllers
+{
+    public class VendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
